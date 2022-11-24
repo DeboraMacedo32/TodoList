db40979dc527941794ec356184efe5dd3418e22d
Debora <deboramacedo26@outlook.com>
2022-11-24 13:02:36 -0300

    add TodoList

---
+++ src/app/app.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  title = 'TodoTarefa';
+  displayedColumns: string[] = ['taskName', 'category',  'comment', 'date1', 'date2','statusTask', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+constructor(private dialog : MatDialog, private api : ApiService){
+
+}
+  ngOnInit(): void {
+    this.getAllTasks();
+  }
+
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%'
+    }).afterClosed().subscribe(val=>{
+      if(val === 'save'){
+        this.getAllTasks();
+      }
+    })
+  }
+
+  getAllTasks(){
+    this.api.getTask()
+    .subscribe({
+      next:(res)=>{
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error:(err)=>{
+        alert("Erro ao buscar os registros! ")
+      }
+    })
+  }
+
+  editTask(row : any){
+    this.dialog.open(DialogComponent,{
+      width: '30%',
+      data:row
+    }).afterClosed().subscribe(val=>{
+      if(val=== 'update'){
+        this.getAllTasks();
+      }
+    })
+
+  }
+  deleteTask(id:number){
+    this.api.deleteTask(id)
+    .subscribe({
+      next:(res=>{
+        alert("Tarefa deletada com sucesso!");
+        this.getAllTasks();
+      }),
+      error:()=>{
+        alert("Erro ao deletar a tarefa!")
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  
+}
+
---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,79 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
+
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent implements OnInit {
+
+   statusLista = ["Concluída", "Não Concluída", "Em Andamento"];
+   tarefaForm !: FormGroup;
+   actionBtn : string = "Salvar"
+
+  constructor(private formBuilder : FormBuilder,
+     private api : ApiService,
+     @Inject(MAT_DIALOG_DATA) public editData : any,
+      private dialogRef : MatDialogRef<DialogComponent>) { }
+
+  ngOnInit(): void {
+
+    this.tarefaForm = this.formBuilder.group({
+      taskName : ['', Validators.required],
+      category : ['', Validators.required],
+      statusTask : ['', Validators.required],
+      comment : ['', Validators.required],
+      date1 : ['', Validators.required],
+      date2 : ['', Validators.required]
+
+    });
+    if(this.editData){
+      this.actionBtn = "Atualizar";
+      this.tarefaForm.controls['taskName'].setValue(this.editData.taskName);
+      this.tarefaForm.controls['category'].setValue(this.editData.category);
+      this.tarefaForm.controls['statusTask'].setValue(this.editData.statusTask);
+      this.tarefaForm.controls['comment'].setValue(this.editData.comment);
+      this.tarefaForm.controls['date1'].setValue(this.editData.date1);
+      this.tarefaForm.controls['date2'].setValue(this.editData.date2);
+      
+    }
+  }
+
+  addTask() {
+   if(!this.editData){
+    if(this.tarefaForm.valid){
+      this.api.postTask(this.tarefaForm.value)
+      .subscribe({
+        next:(res)=>{
+          alert("Tarefa adicionada com sucesso!");
+          this.tarefaForm.reset();
+          this.dialogRef.close('salvar');
+        },
+        error:()=>{
+          alert("Erro ao add a tarefa")
+        }
+      })
+     }
+   }else{
+    this.updateTask()
+
+   }
+  }
+  updateTask(){
+    this.api.putTask(this.tarefaForm.value, this.editData.id)
+    .subscribe({
+      next:(res)=>{
+        alert("Tarefa atualizada com sucesso!");
+        this.tarefaForm.reset();
+        this.dialogRef.close('update');
+      },
+      error:()=>{
+        alert("Erro ao atualizar o registro!")
+      }
+    })
+  }
+}
